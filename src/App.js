018bbd60ed07d3e52b5fd8a154ffa79ce7a4f44e
netsus <cwjcsk@naver.com>
2021-10-24 00:40:00 +0100

    #2.2 useClick - part 1 1. useRef : useRef란 특정 DOM(구조)을 선택할 수 있게끔 해주는 기능 const tomato = useRef();
    setTimeout(() => tomato.current?.focus(), 2000);
    --> tomato 변수의 현재 (current)에 대해, focus하는 기능
    Component DidMount가 너무 빨라서, tomato.current가 극 초반에 존재하지않아 에러발생가능
    --> Optional chaining (?.) 를 사용하여 ? 앞부분(tomato.current)이 null 또는 undefined이면 평가를 멈추고 undefined를 반환하도록 하는 기능 이다.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import { useInput } from './useinput';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
-};
 
 const App = () => {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Coffee"), 3000);
+  const tomato = useRef();
+  setTimeout(() => {
+    tomato.current?.focus();
+  },
+   2000);
   return (
     <div className="App">
       <div>Hi</div>
+      <input ref={tomato} placeholder="union" />
     </div>
   );
 }
