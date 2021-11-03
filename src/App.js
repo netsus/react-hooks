16ffd293e593090d9ec792adb4bd46116e1d0d81
netsus <cwjcsk@naver.com>
2021-11-04 00:51:20 +0900

    #2.3 useConfirm & usePreventLeave - Part1 message="aur you sure" window.confirm(message) --> 윈도우에 해당 메시지로 확인, 취소 창이 뜨고,     확인 누르면 True일때 callback 함수 실행, 취소누르면 rejection함수 실행
    1. useConfirm함수 생성
      1) message, callback 함수, rejection 함수를 매개변수로 받음
      2) window.confirm(message)가 true이면 callback 실행, false이면 rejection 실행
    
    2. App 함수
      1) useConfirm() 실행값을 변수에 넣음.
    버튼 태그의 onClick에 confirmDelete함수 넣음.
      --> const confirmDelete = useConfirm("aur you sure", deleteWorld, abort);

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react';
 
-const useClick = (onClick) => {
-  const element = useRef();
-  useEffect(() => {
-    if(element.current){
-      element.current.addEventListener("click", onClick);
+const useConfirm = (message = "", callback, rejection) => {
+  if (typeof callback !== "function"){
+    return;
+  }
+  const confirmAction = () => {
+    if(window.confirm(message)){
+      callback();
+    } else {
+      rejection();
     }
-    return () => {
-      if(element.current){
-        element.current.removeEventListener("click", onClick);
-      }
-    }
-    },[])
-    
-    return typeof onClick === "function" ? element : undefined;
-  };
+  }
+  return confirmAction;
+}
 
 const App = () => {
-  const sayHello = () => console.log("say hello");
-  const title = useClick(sayHello);
+  const deleteWorld = () => console.log("Deleting the world");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("aur you sure", deleteWorld, abort);
   return (
     <div className="App">
-      <h1 ref={title}>Hi</h1>
+      <button onClick={confirmDelete}>Delete the world</button>
     </div>
   );
 }
